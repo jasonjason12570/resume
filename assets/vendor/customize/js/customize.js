4b8848c0200b4296dda78afa007253fc4468fe7e
Owen <jasonjason12570@gmail.com>
2021-06-01 16:56:44 +0800

    fix

--- assets/vendor/customize/js/customize.js
+++
@@ -1,7 +0,0 @@
-//　
-
-var Today=new Date();
-var birthday=new Date("1998-08-25".replace(/-/g, "\/")); 
-var d=new Date(); 
-var Value = d.getFullYear()-birthday.getFullYear()-((d.getMonth()<birthday.getMonth()|| d.getMonth()==birthday.getMonth() && d.getDate()<birthday.getDate())?1:0);
-document.getElementById("age").innerHTML = Value;
